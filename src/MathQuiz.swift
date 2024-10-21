5eb6b73d888176cda6a6fa17957e06e56ea3ccf9
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-21 19:24:34 +0530

    feat [ src ] : Complete Math Quiz program

--- src/MathQuiz.swift
+++ src/MathQuiz.swift
@@ -6,18 +6,23 @@ let numQuestions = 5
 var score = 0
 
 print("Welcome to the Math Quiz!")
-for _ in 1...numQuestions {
+print("_________________________")
+
+for no in 1...numQuestions {
     let num1 = Int.random(in: 1...10)
     let num2 = Int.random(in: 1...10)
     let correctAnswer = num1 + num2
     
+    print("> Question \(no)")
     print("What is \(num1) + \(num2)?")
+    
     if let answer = readLine(), let userAnswer = Int(answer), userAnswer == correctAnswer {
-        print("Correct!")
+        print("🗸 Correct!")
         score += 1
     } else {
-        print("Wrong! The correct answer is \(correctAnswer).")
+        print("ⓧ Wrong! The correct answer is \(correctAnswer).")
     }
+    print("_________________________")
 }
 
 print("You got \(score) out of \(numQuestions) correct.")
