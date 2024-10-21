d83691dde677061cbd859bf96ee1da157e7e6800
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-21 16:38:12 +0530

    feat [ src ] : Complete Number Guessing Game Program

--- src/NumberGuessingGame.swift
+++ src/NumberGuessingGame.swift
@@ -3,6 +3,7 @@ import Foundation
 // Generate a random number between 1 and 100
 let targetNumber = Int.random(in: 1...100)
 var guess: Int? = nil
+var numberOfTries = 0
 
 print("Welcome to the Number Guessing Game!")
 print("Guess a number between 1 and 100.")
@@ -10,19 +11,28 @@ print("Guess a number between 1 and 100.")
 while guess != targetNumber {
 
     print("Enter your guess: ", terminator: "")
-    
+
     // Read user input
     if let input = readLine(), let userGuess = Int(input) {
         guess = userGuess
+        numberOfTries += 1
+
         // Compare guess with target number
         if guess! < targetNumber {
             print("Too low!")
+
         } else if guess! > targetNumber {
             print("Too high!")
+
         } else {
-            print("Congratulations! You guessed the number.")
+            print("---------------- Congratulations! -----------------")
+            print("You've guessed the number in \(numberOfTries) tries.")
+            print("----------------------------------------------------")
         }
+
     } else {
+
         print("Please enter a valid number.")
+
     }
 }
